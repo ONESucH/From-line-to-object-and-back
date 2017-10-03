2354f9a24b093ee371e53dde26c7a6064333aea7
ilya <onesuch07@gmail.com>
2017-10-03 13:07:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,40 +2,56 @@
 // "{2} - {3} :: {12}" => {columns: [2,3,12], maskSymbols: [' - ',' :: ']}
 
 var reverseObj = {};
-var str = '@234dsfs {223121} - {4}{5}++++ {9}';
+var str = '@234dsfs{100}-{200}{300}++++{9}';
+
+console.log('str ', str);
 
 a(str);
 
 function a(string) {
-    var html = [];
+    var logic = false,
+        columns = [],
+        maskSymbols = [],
+        html = '',
+        html_2 = '';
 
-    var regIx = string.match(/\{.+?}/g);
+    for(var a = 0; a < string.length; a++) {
+        if (!logic) {
+            html_2 = '';
+            html += string[a];
+        } else {
+            html = '';
+            html_2 += string[a];
+        }
 
-    for(var key in regIx) {
-        var rightSymb = regIx[key].slice(0, regIx[key].length-1);
-        var leftSymb = rightSymb.slice(1);
+        if(string[a] === '{') {
+            var clear = html.slice(0, -1);
+            columns.push(clear);
+            reverseObj.columns = columns;
+            logic = true;
+        }
+        if(string[a] === '}') {
+            var clear_2 = html_2.slice(0, -1);
+            maskSymbols.push(clear_2);
+            delete maskSymbols[maskSymbols.indexOf('}')];
+            reverseObj.maskSymbols = maskSymbols;
+            logic = false;
+        }
 
-        html.push(leftSymb);
     }
 
-    reverseObj.column = html;
-    reverseObj.maskSymbol = string.match(/[^a-zA-Z а-яА-Я0-9{}:]/g);
-
-    console.log('Полученный результат первой функции -', reverseObj);
-    b(reverseObj);
+    console.log('Полученный результат первой функции -  ', reverseObj);
+   b(reverseObj);
 }
-
 
 function b(obj) {
     var clearSymbol = '';
 
-    for(var a = 0; a < str.length; a++) {
-        if(typeof(obj.column[a] && obj.maskSymbol[a]) === 'undefined') {
-            delete obj.column[a];
-            delete obj.maskSymbol[a];
-        } else {
-            clearSymbol += '{' + obj.column[a] + '}';
-            clearSymbol += obj.maskSymbol[a];
+    for (var a = 0; a < str.length; a++) {
+        if (obj.columns[a] !== undefined) {
+            clearSymbol += obj.columns[a];
+            clearSymbol += '{' + obj.maskSymbols[a] + '}';
+            clearSymbol.replace(typeof(undefined), '');
         }
     }
 
